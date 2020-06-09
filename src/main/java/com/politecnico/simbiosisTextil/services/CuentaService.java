d9f5f334207ff64b64174ad7b5417deac1a3f52a
MilaMesa <maria_caicedo01152@elpoli.edu.co>
2020-06-08 20:40:39 -0500

    camila
    se validan todos los campos obligatorios y se crea validacion de si existe correo o numero de identificiacion

--- src/main/java/com/politecnico/simbiosisTextil/services/CuentaService.java
+++ src/main/java/com/politecnico/simbiosisTextil/services/CuentaService.java
@@ -1,5 +1,6 @@
 package com.politecnico.simbiosisTextil.services;
 
+import com.mysql.cj.util.StringUtils;
 import com.politecnico.simbiosisTextil.controller.dto.Registro;
 import com.politecnico.simbiosisTextil.entity.CuentaDao;
 import com.politecnico.simbiosisTextil.entity.UsuarioDao;
@@ -7,7 +8,6 @@ import com.politecnico.simbiosisTextil.entity.dao.Cuenta;
 import com.politecnico.simbiosisTextil.entity.dao.TipoIdentificacion;
 import com.politecnico.simbiosisTextil.entity.dao.TipoUsuario;
 import com.politecnico.simbiosisTextil.entity.dao.Usuario;
-import javafx.util.converter.CharacterStringConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -49,32 +49,78 @@ public class CuentaService {
     }
 
     private Cuenta validarYObtenerDatosDelRegistro(Registro registro) {
+        validarDatosDelRegistro(registro);
         Cuenta cuenta = new Cuenta();
         Usuario usuario = new Usuario();
-        if(!existeUsuario(registro.getUsuario())){
-            cuenta.setNombreUsuario(registro.getUsuario());
-            cuenta.setPassword(registro.getPassword());
-            usuario.setNombre(registro.getNombre());
-            usuario.setApellido(registro.getApellido());
-            char[] telefono = new char[7];
-            registro.getTelefono().getChars(0, registro.getTelefono().length(), telefono, 0);
-            usuario.setTelefono(telefono);
-            usuario.setCelular(registro.getCelular());
-            usuario.setCorreo(registro.getCorreo());
-            usuario.setDireccion(registro.getDireccion());
-            usuario.setNombreEmpresa(registro.getNombreEmpresa());
-            usuario.setTipoUsuario(TipoUsuario.valueOf(registro.getTipoUsuario()));
-            usuario.setTipoIdentificacion(TipoIdentificacion.valueOf(registro.getTipoIdentificacion()));
-            usuario.setNumeroIdentificacion(registro.getNumeroIdentificacion());
-            usuarioDao.save(usuario);
-        } else{
-            throw new ResponseStatusException(HttpStatus.CONFLICT,"El nombre de usuario ya existe en el sistema.");
-        }
+        cuenta.setNombreUsuario(registro.getUsuario());
+        cuenta.setPassword(registro.getPassword());
+        usuario.setNombre(registro.getNombre());
+        usuario.setApellido(registro.getApellido());
+        char[] telefono = new char[7];
+        registro.getTelefono().getChars(0, registro.getTelefono().length(), telefono, 0);
+        usuario.setTelefono(telefono);
+        usuario.setCelular(registro.getCelular());
+        usuario.setCorreo(registro.getCorreo());
+        usuario.setDireccion(registro.getDireccion());
+        usuario.setNombreEmpresa(registro.getNombreEmpresa());
+        usuario.setTipoUsuario(TipoUsuario.valueOf(registro.getTipoUsuario()));
+        usuario.setTipoIdentificacion(TipoIdentificacion.valueOf(registro.getTipoIdentificacion()));
+        usuario.setNumeroIdentificacion(registro.getNumeroIdentificacion());
+        usuarioDao.save(usuario);
         cuenta.setUsuario(usuario);
         return cuenta;
     }
 
-    private boolean existeUsuario(String usuario) {
+    private void validarDatosDelRegistro(Registro registro) {
+        validarDatosObligatorios(registro);
+        if (existeNombreUsuario(registro.getUsuario())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "El nombre de usuario ya existe en el sistema.");
+        }
+        if (existeUsuario(registro.getNumeroIdentificacion())) {
+            throw new ResponseStatusException(HttpStatus.FAILED_DEPENDENCY, "La identificacion ingresada ya existe para otra cuenta.");
+        }
+        if (existeCorreoElectronico(registro.getCorreo())) {
+            throw new ResponseStatusException(HttpStatus.FAILED_DEPENDENCY, "El correo ingresado ya existe para otra cuenta.");
+        }
+    }
+
+    private void validarDatosObligatorios(Registro registro) {
+        if (null == registro) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No se recibio la informacion para el registro.");
+        }
+        if (StringUtils.isNullOrEmpty(registro.getCorreo())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No se recibio el correo para el registro.");
+        }
+        if (StringUtils.isNullOrEmpty(registro.getApellido())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No se recibio el apellido para el registro.");
+        }
+        if (StringUtils.isNullOrEmpty(registro.getCelular())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No se recibio el celular para el registro.");
+        }
+        if (StringUtils.isNullOrEmpty(registro.getNombre())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No se recibio el nombre para el registro.");
+        }
+        if (StringUtils.isNullOrEmpty(registro.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No se recibio la contraseña para el registro.");
+        }
+        if (StringUtils.isNullOrEmpty(registro.getTipoIdentificacion())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No se recibio el tipo de identificacion para el registro.");
+        }
+        if (StringUtils.isNullOrEmpty(registro.getTipoUsuario())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No se recibio el tipo de usuario para el registro.");
+        }
+    }
+
+    private boolean existeCorreoElectronico(String correo) {
+        return usuarioDao.findByEmail(correo).isPresent();
+    }
+
+
+    private boolean existeUsuario(long numeroIdentificacion) {
+        return usuarioDao.findById(numeroIdentificacion).isPresent();
+    }
+
+    private boolean existeNombreUsuario(String usuario) {
         return cuentaDao.findById(usuario).isPresent();
     }
 }
