e2ac69535f92b5bdc9e5cdb5242279fa018a4840
MilaMesa <maria_caicedo01152@elpoli.edu.co>
2020-06-10 21:23:58 -0500

    camila
    se enviand tados de perfil

--- src/main/java/com/politecnico/simbiosisTextil/controller/dto/Perfil.java
+++ src/main/java/com/politecnico/simbiosisTextil/controller/dto/Perfil.java
@@ -1,15 +1,28 @@
 package com.politecnico.simbiosisTextil.controller.dto;
 
 public class Perfil {
-    private long id;
+    private long numeroIdentificacion;
+    private String tipoIdentificacion;
     private String nombre;
+    private String telefono;
+    private String celular;
+    private String correo;
+    private String direccion;
 
-    public long getId() {
-        return id;
+    public long getNumeroIdentificacion() {
+        return numeroIdentificacion;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setNumeroIdentificacion(long numeroIdentificacion) {
+        this.numeroIdentificacion = numeroIdentificacion;
+    }
+
+    public String getTipoIdentificacion() {
+        return tipoIdentificacion;
+    }
+
+    public void setTipoIdentificacion(String tipoIdentificacion) {
+        this.tipoIdentificacion = tipoIdentificacion;
     }
 
     public String getNombre() {
@@ -18,5 +31,37 @@ public class Perfil {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
     }
 }
--- src/main/java/com/politecnico/simbiosisTextil/services/UsuarioService.java
+++ src/main/java/com/politecnico/simbiosisTextil/services/UsuarioService.java
@@ -2,13 +2,17 @@ package com.politecnico.simbiosisTextil.services;
 
 import com.politecnico.simbiosisTextil.controller.dto.Perfil;
 import com.politecnico.simbiosisTextil.entity.UsuarioDao;
+import com.politecnico.simbiosisTextil.entity.dao.TipoUsuario;
 import com.politecnico.simbiosisTextil.entity.dao.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -21,8 +25,18 @@ public class UsuarioService {
         if (usuarioOptional.isPresent()) {
             Usuario usuario = usuarioOptional.get();
             Perfil perfil = new Perfil();
-            perfil.setId(usuario.getNumeroIdentificacion());
-            perfil.setNombre(String.format("%s %s", usuario.getNombre(), usuario.getApellido()));
+            perfil.setNumeroIdentificacion(usuario.getNumeroIdentificacion());
+            perfil.setTipoIdentificacion(usuario.getTipoIdentificacion().name());
+            if (TipoUsuario.TALLER.equals(usuario.getTipoUsuario())) {
+                perfil.setNombre(String.format("%s %s", usuario.getNombre(), usuario.getApellido()));
+            } else {
+                perfil.setNombre(usuario.getNombreEmpresa());
+            }
+            StringBuilder sbf = new StringBuilder();
+            perfil.setTelefono(sbf.append(usuario.getTelefono()).toString());
+            perfil.setCelular(usuario.getCelular());
+            perfil.setCorreo(usuario.getCorreo());
+            perfil.setDireccion(usuario.getDireccion());
             return perfil;
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("No se obtuvo usuario con la identificacion %s", identificacion));
